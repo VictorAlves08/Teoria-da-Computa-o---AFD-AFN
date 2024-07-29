eb7502a1c5c37f069c2f11ba3716454e24fa05ab
Gabriel Mota <51803858+gslmota@users.noreply.github.com>
2024-07-29 17:45:06 -0300

    Update app.py

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 sys.path.insert(0, project_root)
 
-from automata_app import AutomataApp  # Certifique-se de que o nome do arquivo é 'automata_app.py'
+from automata_app import AutomataApp  
 
 if __name__ == "__main__":
     AutomataApp()
