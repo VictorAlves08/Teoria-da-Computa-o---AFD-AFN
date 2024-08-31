58122eb8782f207a0c0e7d7d568d8e38e48b6861
VictorAlves08 <victoralvees17@gmail.com>
2024-08-31 20:52:56 -0300

    minor rollback

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,4 +1,3 @@
-from automata_app import AutomataApp
 import sys
 import os
 
@@ -6,6 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 sys.path.insert(0, project_root)
 
+from automata_app import AutomataApp
 
 if __name__ == "__main__":
     AutomataApp()
